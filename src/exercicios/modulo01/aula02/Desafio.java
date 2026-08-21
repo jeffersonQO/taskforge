8c2a579c56f0a0fccca22b8d9e1a15f02b51b198
jefferson.queiroz <jeffqueiroz20@outlook.com>
2026-08-20 22:38:38 -0300

    Melhorando código e alterando status pro padrão da aula.

--- src/exercicios/modulo01/aula02/Desafio.java
+++ src/exercicios/modulo01/aula02/Desafio.java
@@ -8,35 +8,7 @@ void main() {
 
         switch(opcao) {
             case "1" -> {
-                String nome = IO.readln("Nome da tarefa: ");
-                String descricao = IO.readln("Descrição: ");
-                int prioridade;
-
-                do {
-                    prioridade = Integer.parseInt(IO.readln("Prioridade (1 a 5): "));
-                    if (prioridade < 1 || prioridade > 5) {
-                        IO.println("Prioridade inválida.");
-                    }
-                } while (prioridade < 1 || prioridade > 5);
-
-                double horasEstimadas = Double.parseDouble(IO.readln("Horas estimadas: "));
-                boolean concluida = Boolean.parseBoolean(IO.readln("Concluída: "));
-                String responsavel = IO.readln("Responsavel: ");
-
-                IO.println("");
-                IO.println("------ TAREFA CRIADA ------");
-                IO.println("Tarefa:     " + nome);
-                IO.println("Descrição:  " + descricao);
-                IO.println("Responsável: " + responsavel);
-                IO.print("Prioridade: ");
-                for(int i = 0; i < prioridade; i++) {
-                    IO.print("*");
-                }
-                IO.println();
-                IO.println("Horas Estimadas: " + horasEstimadas);
-                IO.println("Status: " + (concluida ? "Concluída" : "Pendente"));
-                IO.println("---------------------------");
-
+                criarTarefa();
                 numTarefas++;
             }
             case "2" -> IO.println("Tarefas criada até o momento: " + numTarefas);
@@ -45,4 +17,56 @@ void main() {
         }
     }
     while (!opcao.equals("3"));
+}
+
+void criarTarefa() {
+    String nome = IO.readln("Nome da tarefa: ");
+    String descricao = IO.readln("Descrição: ");
+    int prioridade;
+
+    do {
+        prioridade = Integer.parseInt(IO.readln("Prioridade (1 a 5): "));
+        if (prioridade < 1 || prioridade > 5) {
+            IO.println("Prioridade inválida.");
+        }
+    } while (prioridade < 1 || prioridade > 5);
+
+    double horasEstimadas = Double.parseDouble(IO.readln("Horas estimadas: "));
+
+    int status;
+    do {
+        IO.println("Escolha uma opção: ");
+        IO.println("1 - Pendente");
+        IO.println("2 - Em Andamento");
+        IO.println("3 - Concluída");
+        IO.println("4 - Cancelada");
+        status = Integer.parseInt(IO.readln());
+        if (status < 1 || status > 4) {
+            IO.println("Status inválido.");
+        }
+    } while (status < 1 || status > 4);
+
+    String responsavel = IO.readln("Responsavel: ");
+
+    IO.println("");
+    IO.println("------ TAREFA CRIADA ------");
+    IO.println("Tarefa:     " + nome);
+    IO.println("Descrição:  " + descricao);
+    IO.println("Responsável: " + responsavel);
+    IO.print("Prioridade: ");
+    for(int i = 0; i < prioridade; i++) {
+        IO.print("*");
+    }
+    IO.println();
+    IO.println("Horas Estimadas: " + horasEstimadas);
+    String statusNome = "";
+    switch (status) {
+        case 1 -> statusNome = "Pendente";
+        case 2 -> statusNome = "Em Andamento";
+        case 3 -> statusNome = "Concluída";
+        case 4 -> statusNome = "Cancelada";
+        default -> statusNome = "Status Inválido";
+    }
+    IO.println("Status: " + statusNome);
+    IO.println("---------------------------");
 }
